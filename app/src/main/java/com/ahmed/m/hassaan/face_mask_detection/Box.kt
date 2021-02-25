3067a3f2899c940237551fd09cdebf646835f302
Ahmed M Hassaan <aabojana09@gmail.com>
2021-02-25 20:22:42 +0200

    Android  Face Mask Detection

---
+++ app/src/main/java/com/ahmed/m/hassaan/face_mask_detection/Box.kt
@@ -0,0 +1,5 @@
+package com.ahmed.m.hassaan.face_mask_detection
+
+import android.graphics.RectF
+
+class Box(val rectF: RectF, val label: String, val isMask: Boolean)
---
+++ app/src/main/java/com/ahmed/m/hassaan/face_mask_detection/OverlayView.kt
@@ -0,0 +1,35 @@
+package com.ahmed.m.hassaan.face_mask_detection
+
+import android.graphics.Canvas
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+
+class OverlayView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    var boundingBox: MutableList<Box> = mutableListOf()
+    var paint = Paint()
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 5f
+        paint.strokeCap = Paint.Cap.ROUND
+        paint.strokeJoin = Paint.Join.ROUND
+        paint.strokeMiter = 120f
+        boundingBox.forEach { box ->
+            if (box.isMask) {
+                paint.color = Color.GREEN
+            } else {
+                paint.color = Color.RED
+            }
+            paint.setTextAlign(Paint.Align.LEFT)
+            canvas.drawText(box.label, box.rectF.left, box.rectF.top - 9F, paint)
+            canvas.drawRoundRect(box.rectF, 2F, 2F, paint)
+        }
+    }
+}
